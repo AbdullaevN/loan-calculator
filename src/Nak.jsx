9e0d9a282995080d91858f1403cb47c143bdaec8
AbdullaevN <nurlanabdullaev9820@gmail.com>
2023-02-24 13:40:09 +0600

    nak change save

--- src/Nak.jsx
+++ src/Nak.jsx
@@ -9,7 +9,23 @@ import { convert } from "exchange-rates-api";
 import axios from "axios";
 
 const savingPercentage = [10, 15, 20, 25, 30, 35, 40, 45, 50];
-const monthOptions = [3, 6, 9, 12, 24];
+const monthOptions = [3, 6, 9, 12, 18, 24];
+
+const edinper = [
+  {
+    title: "four",
+    value: 4,
+  },
+  {
+    title: "uch jarym",
+    value: 3.5,
+  },
+  {
+    title: "uch",
+    value: 3,
+  },
+];
+
 // const currencySelect = ["сом", "сум", "рубль", "usd", "тенге"];
 const currencySelect = [
   {
@@ -49,15 +65,11 @@ const minValueInDollars = 4000;
 const maxValueInDollars = 150000;
 
 export const Nako = () => {
+  const [pers, setPers] = useState();
   const [price, setPrice] = useState("3000000");
   const [srok, setSrok] = useState("2");
-  const [selectedFirstContributionOption, setSelectedFirstContributionOption] =
-    useState(savingPercentage[0]);
-  const [firstContributionValue, setFirstContributionValue] = useState(
-    (+price * selectedFirstContributionOption) / 100
-  );
-
   const [currency, setCurrency] = useState(currencySelect[0]);
+  const [firstF, setFirstF] = useState("kk");
 
   const edinPercent = 5;
 
@@ -66,6 +78,8 @@ export const Nako = () => {
   var money = 3000000;
   var tallage = 14;
 
+  const [selectedFirstContributionOption, setSelectedFirstContributionOption] =
+    useState(savingPercentage[2]);
   const [selectedYearOption, setSelectedYearOption] = useState(monthOptions[0]);
 
   const firstContribution =
@@ -121,7 +135,7 @@ export const Nako = () => {
   //
 
   //ежемесячный платеж
-  const percent = 15;
+  const percent = 5;
   const month = selectedYearOption * 12;
 
   let res = Number(price * percent) / 100;
@@ -161,49 +175,102 @@ export const Nako = () => {
   const getSrok = (val) => {
     setSrok(srokOptions[val] || "");
   };
-  console.log(selectedFirstContributionOption);
+
+  const options = [3, 6, 9, 18, 24];
+  const [selectedOption, setSelectedOption] = useState("");
+  const [waitTime, setWaitTime] = useState("");
+
+  const handleOptionChange = (event) => {
+    const value = parseInt(event.target.value);
+    let time = 0;
+
+    switch (value) {
+      case 3:
+        time = 4;
+        break;
+      case 9:
+        time = 3.5;
+        break;
+      case 24:
+        time = 3;
+        break;
+      default:
+        console.log("Выберите значение 3, 9 или 24.");
+    }
+
+    setSelectedOption(value);
+  };
+
+  const handleChosenPeriod = (event) => {
+    setSelectedYearOption(event.target.value);
+
+    const value = parseInt(event.target.value);
+
+    let time = 0;
+
+    switch (value) {
+      case 3:
+        time = 5;
+        break;
+      case 6:
+        time = 5;
+        break;
+      case 9:
+        (time = 4), 5;
+        break;
+      case 12:
+        (time = 4), 5;
+        break;
+      case 18:
+        time = 4;
+        break;
+      case 24:
+        time = 4;
+        break;
+      default:
+        console.log("Выберите значение 3, 9 или 24.");
+    }
+
+    setWaitTime(time);
+  };
   return (
     <>
-      <>
-        <>
-          <div className="home">
-            <div className="home-content">
-              <div className="left">
-                <h1>Накопительная</h1>
-                <div></div>
-                <div>Сумма</div>
-                <Form.Control
-                  value={price}
-                  className="summa-select"
-                  min={`${minValueInChosenCurrency}`}
-                  max={`${maxValueInChosenCurrency}`}
-                  type="number"
-                  onChange={(event) => setPrice(event.target.value)}
+      <div className="home">
+        <div className="home-content">
+          <div className="left">
+            <h1>Накопительная</h1>
+            <div></div>
+            <div>Сумма</div>
+            <Form.Control
+              value={price}
+              className="summa-select"
+              min={`${minValueInChosenCurrency}`}
+              max={`${maxValueInChosenCurrency}`}
+              type="number"
+              onChange={(event) => setPrice(event.target.value)}
 
-                  // onChange={(e) => console.log(e ===)}
-                />
+              // onChange={(e) => console.log(e ===)}
+            />
 
-                <div>
-                  <div className="content-price">
-                    <div className="content-price-block">
-                      {selectedCurrency}
-                    </div>
-                    <div className="content-price-block">{price}</div>
-                  </div>
+            <div>
+              <div className="content-price">
+                <div className="content-price-block">{selectedCurrency}</div>
+                <div className="content-price-block">{price}</div>
+              </div>
 
-                  <Form.Select
-                    onChange={(event) => {
-                      console.log("som", event.target.value);
+              <Form.Select
+                onChange={(event) => {
+                  console.log("som", event.target.value);
 
-                      setSelectedCurrency(event.target.value);
-                    }}
-                  >
-                    {CurrencyOptions.map(({ title }) => (
-                      <option key={title}>{title}</option>
-                    ))}
-                  </Form.Select>
+                  setSelectedCurrency(event.target.value);
+                }}
+              >
+                {CurrencyOptions.map(({ title }) => (
+                  <option key={title}>{title}</option>
+                ))}
+              </Form.Select>
 
-                  {/* <input
+              {/* <input
                     type="range"
                     className="range"
                     min={`${minValueInChosenCurrency}`}
@@ -211,108 +278,118 @@ export const Nako = () => {
                     value={price}
                     onChange={(event) => setPrice(event.target.value)}
                   /> */}
-                </div>
+            </div>
 
-                {/* */}
+            {/* */}
 
-                <div>Первоначальный взнос</div>
-                <Form.Select
-                  className="select"
-                  name="% Первоначальный взнос"
-                  onChange={(event) =>
-                    setSelectedFirstContributionValue(event.target.value)
-                  }
-                >
-                  {savingPercentage.map((firstContributionOption) => (
-                    <option key={firstContributionOption}>
-                      {firstContributionOption}
+            <div>Первоначальный взнос</div>
+            <Form.Select
+              className="select"
+              name="% Первоначальный взнос"
+              onChange={(event) =>
+                setSelectedFirstContributionValue(event.target.value)
+              }
+            >
+              {savingPercentage.map((firstContributionOption) => (
+                <option key={firstContributionOption}>
+                  {firstContributionOption}
+                </option>
+              ))}
+            </Form.Select>
+
+            {/*     2*/}
+            <div>Срок накопления</div>
+            <Form.Select
+              className="select"
+              name="Срок"
+              onChange={(event) => handleChosenPeriod(event)}
+            >
+              {monthOptions.map((yearOption) => (
+                <option key={yearOption}>{yearOption}</option>
+              ))}
+            </Form.Select>
+          </div>
+          <div className="right table-responsive">
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Название </th>
+                  <th>Результат</th>
+                  <th>Валюта</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="td">Сумма финансирования:</td>
+                  <td>{price} </td>
+                  <td>{selectedCurrency} </td>
+                </tr>
+                <tr>
+                  <td className="td">Первоначальный взнос:</td>
+                  <td>{firstContribution}</td>
+                  <td>{selectedCurrency} </td>
+                </tr>
+
+                <tr>
+                  <td className="td">Единаразовый взнос: </td>
+                  {/* <td colSpan={2}>{newResulEdinPercent}</td> */}
+                  <td>{newResulEdinPercent}</td>
+                  <td>{selectedCurrency} </td>
+                </tr>
+                {/* <tr>
+                  <td className="td">Ежемесячный платеж:</td>
+                  <td>{total}</td>
+                  <td>{selectedCurrency} </td>
+                </tr> */}
+                <tr>
+                  <td className="td">Сумма ПВ и ЕВ:</td>
+                  <td>{summaPvEv}</td>
+                  <td>{selectedCurrency} </td>
+                </tr>
+                <tr>
+                  <td className="td">Срок ожидания:</td>
+                  <td>{waitTime > 0 && <div>{waitTime} </div>}</td>
+                  <td>месяцa(цев) </td>
+                </tr>
+                <tr>
+                  <td className="td">Сумма финансирования:</td>
+                  <td>{leftover}</td>
+                  <td>{selectedCurrency} </td>
+                </tr>
+                <tr>
+                  <td className="td">Срок накопления:</td>
+                  <td>{selectedYearOption}</td>
+                  <td> месяцa(цев) </td>
+                </tr>
+              </tbody>
+            </Table>
+
+            {/* <div>
+              <label>
+                Выберите значение:
+                <select value={selectedOption} onChange={handleOptionChange}>
+                  <option value="">--Выберите--</option>
+                  {options.map((option) => (
+                    <option key={option} value={option}>
+                      {option}
                     </option>
                   ))}
-                </Form.Select>
-
-                {/*     2*/}
-                <div>Срок накполения</div>
-                <Form.Select
-                  className="select"
-                  name="Срок"
-                  onChange={(event) =>
-                    setSelectedYearOption(event.target.value)
-                  }
-                >
-                  {monthOptions.map((yearOption) => (
-                    <option key={yearOption}>{yearOption}</option>
-                  ))}
-                </Form.Select>
-              </div>
-              <div className="right table-responsive">
-                <Table striped bordered hover>
-                  <thead>
-                    <tr>
-                      <th>Название </th>
-                      <th>Результат</th>
-                      <th>Валюта</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td className="td">Сумма финансирования:</td>
-                      <td>{price} </td>
-                      <td>{selectedCurrency} </td>
-                    </tr>
-                    <tr>
-                      <td className="td">Первоначальный взнос:</td>
-                      <td>{firstContribution}</td>
-                      <td>{selectedCurrency} </td>
-                    </tr>
-
-                    <tr>
-                      <td className="td">Единаразовый взнос: </td>
-                      {/* <td colSpan={2}>{newResulEdinPercent}</td> */}
-                      <td>{newResulEdinPercent}</td>
-                      <td>{selectedCurrency} </td>
-                    </tr>
-                    <tr>
-                      <td className="td">Ежемесячный платеж:</td>
-                      <td>{total}</td>
-                      <td>{selectedCurrency} </td>
-                    </tr>
-                    <tr>
-                      <td className="td">Сумма ПВ и ЕВ:</td>
-                      <td>{summaPvEv}</td>
-                      <td>{selectedCurrency} </td>
-                    </tr>
-                    <tr>
-                      <td className="td">Срок ожидания:</td>
-                      <td>{srok}</td>
-                      <td>{selectedCurrency} </td>
-                    </tr>
-                    <tr>
-                      <td className="td">Сумма финансирования:</td>
-                      <td>{leftover}</td>
-                      <td>{selectedCurrency} </td>
-                    </tr>
-                    <tr>
-                      <td className="td">Срок накопления:</td>
-                      <td>{selectedYearOption}</td>
-                      <td>{selectedCurrency} </td>
-                    </tr>
-                  </tbody>
-                </Table>
-
-                <div>
-                  <ol>
-                    {[...Array(Number(selectedYearOption)).keys()].map((id) => (
-                      <li key={id}>
-                        {(result / selectedYearOption).toFixed(2)}
-                      </li>
-                    ))}
-                  </ol>
-                </div>
-              </div>
+                </select>
+              </label>
+              {waitTime > 0 && (
+                <p>Ваш срок ожидания составляет {waitTime} месяцев.</p>
+              )}
+            </div> */}
+            <div>
+              <ol>
+                {[...Array(Number(selectedYearOption)).keys()].map((id) => (
+                  <li key={id}>{(result / selectedYearOption).toFixed(2)}</li>
+                ))}
+              </ol>
             </div>
           </div>
-        </>
-      </>
+        </div>
+      </div>
     </>
   );
 };
